f6bcc994094cb8596a4976558384d76ed717b9c1
dominicmena <mena.dominic@gmail.com>
2024-06-11 18:01:38 -0500

    created route for bouth snippet and keywords

--- brandbot_api.py
+++ brandbot_api.py
@@ -9,11 +9,18 @@ app = FastAPI()
 @app.get("/generate_snippet")
 async def generate_snippet_api(prompt: str):
      snippet = generate_branding_snippet(prompt)
-     return {"snippet": snippet}
+     return {"snippet": snippet, "keywords": []}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/generate_keywords")
+async def generate_keywords_api(prompt: str):
+     keywords = generate_keywords(prompt)
+     return {"snippet": None, "keywords": keywords}
+
+@app.get("/generate_snippet_and_keywords")
+async def generate_keywords_api(prompt: str):
+     snippet = generate_branding_snippet(prompt)
+     keywords = generate_keywords(prompt)
+     return {"snippet": snippet, "keywords": keywords}
 
 # RUN SERVER: fastapi dev branbot_api.py /// http://127.0.0.1:8000/docs for dashboard
