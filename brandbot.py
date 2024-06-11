56883a0a3cf3286b90aa374f15aa2b2807784d77
dominicmena <mena.dominic@gmail.com>
2024-06-11 17:24:08 -0500

    successfully installed fastapi and running server with fastapi dev brandbot_api.py

--- brandbot.py
+++ brandbot.py
@@ -13,11 +13,9 @@ load_dotenv()
 
 warnings.filterwarnings("ignore", category=NotOpenSSLWarning)
 
-MAX_INPUT_LENGTH = 12
+MAX_INPUT_LENGTH = 32
 
 def main():
-    print("Running brandbot!")
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", "-i", type=str, required=True)
     args = parser.parse_args()
@@ -25,10 +23,8 @@ def main():
 
     print(f"User input: {user_input}")
     if validate_length(user_input):
-        branding_result = generate_branding_snippet(user_input)
-        keywords_result = generate_keywords(user_input)
-        print(branding_result)
-        print(keywords_result)
+        generate_branding_snippet(user_input)
+        generate_keywords(user_input)
 
     else:
         raise ValueError(f"Input length is too long. Must be under {MAX_INPUT_LENGTH}. Submitted input is {user_input}")
@@ -38,12 +34,13 @@ def validate_length(prompt: str) -> bool:
     return len(prompt) <= MAX_INPUT_LENGTH
 
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 
 def generate_keywords(prompt: str) -> List[str]:
-
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     enriched_prompt = f"Generate related branding keywords for {prompt}: "
+    print(enriched_prompt)
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -64,12 +61,14 @@ def generate_keywords(prompt: str) -> List[str]:
     keywords_array = [k.lower().strip() for k in keywords_array]
     keywords_array = [k for k in keywords_array if len(k) > 0]
 
+    print(f"Keywords: {keywords_text}")
     return keywords_array
 
 
 def generate_branding_snippet(prompt: str) -> str:
-
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     enriched_prompt = f"Generate upbeat branding snippet for {prompt}: "
+    print(enriched_prompt)
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -92,6 +91,7 @@ def generate_branding_snippet(prompt: str) -> str:
     if last_char not in {".", "!", "?"}:
         branding_text += "..."
 
+    print(f"Branding: {branding_text}")
     return branding_text
 
 
