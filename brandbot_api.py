bd2629650dd1ead4b26b79662ade24c370b2d24f
dominicmena <mena.dominic@gmail.com>
2024-06-11 17:34:07 -0500

    updated route to generate snippet and importing snippet and keyword functions and passing prompts correctly, testing in the api dashboard

--- brandbot_api.py
+++ brandbot_api.py
@@ -1,13 +1,15 @@
 from typing import Union
+from brandbot import generate_branding_snippet, generate_keywords
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/generate_snippet")
+async def generate_snippet_api(prompt: str):
+     snippet = generate_branding_snippet(prompt)
+     return {"snippet": snippet}
 
 
 @app.get("/items/{item_id}")
