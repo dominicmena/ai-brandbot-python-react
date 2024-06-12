1e6e8c1dec8b14d9c8edb41392c2745db726306c
dominicmena <mena.dominic@gmail.com>
2024-06-11 21:24:32 -0500

    tested input length validation, working perfectly

--- brandbot_api.py
+++ brandbot_api.py
@@ -1,26 +1,39 @@
 from typing import Union
 from brandbot import generate_branding_snippet, generate_keywords
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+MAX_INPUT_LENGTH = 32
 
 
 @app.get("/generate_snippet")
 async def generate_snippet_api(prompt: str):
-     snippet = generate_branding_snippet(prompt)
-     return {"snippet": snippet, "keywords": []}
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    return {"snippet": snippet, "keywords": []}
 
 
 @app.get("/generate_keywords")
 async def generate_keywords_api(prompt: str):
-     keywords = generate_keywords(prompt)
-     return {"snippet": None, "keywords": keywords}
+    validate_input_length(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": None, "keywords": keywords}
+
 
 @app.get("/generate_snippet_and_keywords")
 async def generate_keywords_api(prompt: str):
-     snippet = generate_branding_snippet(prompt)
-     keywords = generate_keywords(prompt)
-     return {"snippet": snippet, "keywords": keywords}
+    validate_input_length(prompt)
+    snippet = generate_branding_snippet(prompt)
+    keywords = generate_keywords(prompt)
+    return {"snippet": snippet, "keywords": keywords}
+
+
+def validate_input_length(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(
+            status_code=400, detail=f"Input length is too long. must be under {MAX_INPUT_LENGTH} characters")
+
 
 # RUN SERVER: fastapi dev branbot_api.py /// http://127.0.0.1:8000/docs for dashboard
